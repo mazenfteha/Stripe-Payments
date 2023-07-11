0b8d24035f1030cc2a7e077c151657f19dac6b05
mazenfteha <mazn34903@gmail.com>
2023-07-10 22:44:24 -0700

    js logic and pay route

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const stripePublickey = process.env.STRIPE_PUBLIC_KEY
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const stripe = require('stripe')
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/store', (req, res) => {
@@ -20,7 +22,26 @@ app.get('/store', (req, res) => {
         }
         else{
             res.render('store.ejs', {
+                stripePublickey: stripePublickey,
                 items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+app.post('/purchase', (req,res) => {
+    fs.readFile('items.json', function(error, data) {
+        if(error) {
+            res.status(500).end()
+        } else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            req.body.items.foreach(function(item) {
+                const itemJson = itemsArray.find(function(i) {
+                    return i.id == item.id
+                })
+                total = total + itemJson.price * item.quntity
             })
         }
     })
